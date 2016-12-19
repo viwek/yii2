7f5bb2ea3b2a39adcd76d5cb9624ba6212708116
viwek <viwek@outlook.com>
2016-12-19 13:22:54 +0545

    fixed grammar

--- framework/base/ViewContextInterface.php
+++ framework/base/ViewContextInterface.php
@@ -8,7 +8,7 @@
 namespace yii\base;
 
 /**
- * ViewContextInterface is the interface that should implemented by classes who want to support relative view names.
+ * ViewContextInterface is the interface that should be implemented by classes who want to support relative view names.
  *
  * The method [[getViewPath()]] should be implemented to return the view path that may be prefixed to a relative view name.
  *
